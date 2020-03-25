f433d3a2a0e6a458d333359c2847ec26b6d1c290
nithishna <nithishna@gmail.com>
2020-03-25 12:07:36 +0000

    Reading input file - success test case

--- src/main/java/com/mobiquity/packer/FileReader.java
+++ src/main/java/com/mobiquity/packer/FileReader.java
@@ -3,27 +3,45 @@ package com.mobiquity.packer;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InvalidObjectException;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.List;
 
+import com.mobiquity.model.Item;
 import com.mobiquity.model.Package;
 
 public class FileReader {
 
 	public static List<Package> readFile(String path) throws IOException {
 		BufferedReader reader = null;
+		List<Package> packages = new ArrayList<>();
 		try {
-			reader = new BufferedReader(new java.io.FileReader(path));
+			reader = new BufferedReader(new java.io.FileReader(path, StandardCharsets.UTF_8));
 			String line = reader.readLine();
-			while(line != null) {
+			while (line != null) {
 				if (!line.matches("\\d* :( \\(\\d*,\\d*(\\.\\d*)?,\\€\\d*\\))*")) {
-					throw new InvalidObjectException("File format incorrect");				
-				}					
+					throw new InvalidObjectException("File format incorrect");
+				}
+				String[] values = line.split(" : ");
+				double weight = Double.parseDouble(values[0]);
+				Package pack = new Package(weight);
+				String[] elements = values[1].split(" ");
+				List<Item> items = new ArrayList<>();
+				for (String element : elements) {
+					element = element.substring(1, element.length() - 1);
+					String[] itemDetails = element.split(",");
+					Item item = new Item(Integer.parseInt(itemDetails[0]), Double.parseDouble(itemDetails[1]),
+							Double.parseDouble(itemDetails[2].substring(1)));
+					items.add(item);
+				}
+				pack.setItems(items);
+				packages.add(pack);
 				line = reader.readLine();
 			}
-		}finally {
-			if(reader != null)
+		} finally {
+			if (reader != null)
 				reader.close();
 		}
-		return null;
+		return packages;
 	}
 }
--- src/test/java/com/mobiquity/packer/FileReaderTest.java
+++ src/test/java/com/mobiquity/packer/FileReaderTest.java
@@ -2,23 +2,44 @@ package com.mobiquity.packer;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import com.mobiquity.model.Package;
 
 public class FileReaderTest {
 
 	@Test
 	public void readFile_NotFound() {
 		String filePath = "invalidFilePath";
-		Assertions.assertThrows(IOException.class, ()->FileReader.readFile(filePath));
+		Assertions.assertThrows(IOException.class, () -> FileReader.readFile(filePath));
 	}
-	
+
 	@Test
 	public void readFile_InvalidFormat() {
 		File file = new File("src/test/resources/example_input_invalid_format");
 		String path = file.getAbsolutePath();
-		Assertions.assertThrows(IOException.class, ()->FileReader.readFile(path));
+		Assertions.assertThrows(IOException.class, () -> FileReader.readFile(path));
+	}
+
+	@Test
+	public void readFile_InvalidEncryption() {
+		File file = new File("src/test/resources/example_input_invalid_encoding");
+		String path = file.getAbsolutePath();
+		Assertions.assertThrows(IOException.class, () -> FileReader.readFile(path));
 	}
 	
+	@Test
+	public void readFile_Success() throws IOException {
+		File file = new File("src/test/resources/example_input");
+		String path = file.getAbsolutePath();
+		List<Package> packs = FileReader.readFile(path);
+		Assertions.assertEquals(packs.size(), 4);
+		Assertions.assertEquals(packs.get(0).getMaxWeight(), 81);
+		Assertions.assertEquals(packs.get(0).getItems().get(0).getIndex(), 1);
+		Assertions.assertEquals(packs.get(0).getItems().get(0).getCost(), 45);
+		Assertions.assertEquals(packs.get(0).getItems().get(0).getWeight(), 53.38);
+	}
 }
